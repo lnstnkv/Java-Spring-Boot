c1b0673f4dd2d32bd828fda52f3ead41580261cf
Elena Sitnikova <lnstnkv2002@gmail.com>
2022-03-18 22:48:16 +0700

    add parser

--- src/main/java/ru/tsu/hits/Application.java
+++ src/main/java/ru/tsu/hits/Application.java
@@ -1,7 +1,26 @@
 package ru.tsu.hits;
 
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Ghbdt");
+        if(args.length<1){
+            throw new IllegalArgumentException("Вы не ввели поисковую строку");
+        }
+
+        var searchString=args[0];
+        var csvStream=Application.class.getResourceAsStream("/tasks.csv");
+        var tasks= new CsvToBeanBuilder<TaskCsv>(new InputStreamReader(Objects.requireNonNull(csvStream)))
+                .withSeparator(',')
+                .withType(TaskCsv.class)
+                .withSkipLines(1)
+                .build()
+                .parse();
+        tasks.stream()
+                .filter(taskCsv -> taskCsv.getName().contains(searchString))
+                .forEach(System.out::println);
     }
 }
---
+++ src/main/java/ru/tsu/hits/TaskCsv.java
@@ -0,0 +1,25 @@
+package ru.tsu.hits;
+
+
+import com.opencsv.bean.CsvBindByPosition;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class TaskCsv {
+    @CsvBindByPosition(position = 0)
+    private Long id;
+
+    @CsvBindByPosition(position = 1)
+     private String name;
+
+    @CsvBindByPosition(position = 2)
+     private String performer;
+
+    @CsvBindByPosition(position = 3)
+     private String creationDate;
+
+}
