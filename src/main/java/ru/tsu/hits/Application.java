6a79016e0d11c94fe2a0480793a3918f3e20e4d3
Elena Sitnikova <lnstnkv2002@gmail.com>
2022-03-19 00:03:28 +0700

    add write file

--- src/main/java/ru/tsu/hits/Application.java
+++ src/main/java/ru/tsu/hits/Application.java
@@ -2,9 +2,11 @@ package ru.tsu.hits;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -20,9 +22,37 @@ public class Application {
                 .withSkipLines(1)
                 .build()
                 .parse();
-        tasks.stream()
+         ArrayList<TaskCsv> tasksList = tasks.stream()
                 //.filter(taskCsv -> taskCsv.getName().contains(searchString))
-                .sorted(Comparator.comparing(TaskCsv::getName))
-                .forEach(System.out::println);
+                .sorted(Comparator.comparing(taskCsv->taskCsv.getCreationDate().contains(searchString)))
+                 .collect(ArrayList::new, // создаем ArrayList
+                         ArrayList::add, // добавляем в список элемент
+                         ArrayList::addAll);
+                //.forEach(System.out::println);
+        System.out.println(tasksList);
+        tasksList.forEach(s -> System.out.println(s));
+        try(FileWriter writer = new FileWriter("tasks.txt", false))
+        {
+            for(TaskCsv task : tasksList){
+                writer.write("Тип задачи: " + task.getTypeTasks()+'\n');
+                writer.append("Название задачи: " + task.getName()+'\n');
+                writer.append("Автор: " + task.getAuthor()+'\n');
+                writer.append("Исполнитель " + task.getPerformer()+'\n');
+                writer.append("Приоритет: " + task.getPriority()+'\n');
+                writer.append("Дата создания: " + task.getCreationDate()+'\n');
+                writer.append('\n');
+                writer.append('\n');
+            }
+
+
+
+
+
+            writer.flush();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
     }
 }
