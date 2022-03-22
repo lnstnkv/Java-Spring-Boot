1684a3c88e180dceac4c039732a563beee117752
Elena Sitnikova <lnstnkv2002@gmail.com>
2022-03-22 16:44:34 +0700

    test commit

--- src/main/java/ru/tsu/hits/springdb1/controller/UserController.java
+++ src/main/java/ru/tsu/hits/springdb1/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.tsu.hits.springdb1.controller;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
